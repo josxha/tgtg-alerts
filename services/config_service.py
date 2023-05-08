f0633cfadb0b6126172eafec8ba30029fdcacdc7
josxha <34318751+josxha@users.noreply.github.com>
2023-05-08 11:04:33 +0200

    improve handling missing env variables

--- services/config_service.py
+++ services/config_service.py
@@ -4,19 +4,26 @@ import os
 class ConfigService:
     def __init__(self):
         # too good to go
+        self.email = None
         if 'TGTG_EMAIL' in os.environ:
             self.email = os.environ['TGTG_EMAIL']
+        self.access_token = None
         if 'TGTG_ACCESS_TOKEN' in os.environ:
             self.access_token = os.environ['TGTG_ACCESS_TOKEN']
+        self.refresh_token = None
         if 'TGTG_REFRESH_TOKEN' in os.environ:
             self.refresh_token = os.environ['TGTG_REFRESH_TOKEN']
+        self.user_id = None
         if 'TGTG_USER_ID' in os.environ:
             self.user_id = os.environ['TGTG_USER_ID']
+        self.cookie = None
         if 'TGTG_COOKIE' in os.environ:
             self.cookie = os.environ['TGTG_COOKIE']
         # telegram
+        self.tg_bot_token = None
         if 'TELEGRAM_BOT_TOKEN' in os.environ:
             self.tg_bot_token = os.environ['TELEGRAM_BOT_TOKEN']
+        self.tg_chat_id = None
         if 'TELEGRAM_CHAT_ID' in os.environ:
             self.tg_chat_id = os.environ['TELEGRAM_CHAT_ID']
 
--- services/tgtg_service.py
+++ services/tgtg_service.py
@@ -7,9 +7,16 @@ class TgtgService:
     def __init__(self, config: ConfigService):
         self.client = None
         if not config.has_token():
+            if config.email is None or config.email == "":
+                print("Please set the TGTG_EMAIL environment variable to get a new session or the other TGTG_* "
+                      "variables with your session.")
+                exit(1)
             self.client = TgtgClient(email=config.email)
             credentials = self.client.get_credentials()
-            print(credentials)
+            print(f"TGTG_ACCESS_TOKEN:\n{credentials['access_token']}\n\n"
+                  f"TGTG_REFRESH_TOKEN:\n{credentials['refresh_token']}\n\n"
+                  f"TGTG_USER_ID:\n{credentials['TGTG_USER_ID']}\n\n"
+                  f"TGTG_COOKIE:\n{credentials['TGTG_COOKIE']}\n")
             exit(0)
         self.client = TgtgClient(
             access_token=config.access_token,
